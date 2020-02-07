6bbb8bb86c0befd6edd68c67c02a5825cec296af
Jessx3x3 <j.vasquez12@ufromail.cl>
2020-02-07 10:36:40 -0300

    Second Part

--- 1942.js
+++ 1942.js
@@ -4,6 +4,12 @@ var hero= {
 }
 var enemies = [{x:150, y:130}, {x:250, y:130}, {x:350, y:130}, {x:450, y:130},
                {x:200, y:80}, {x:300, y:80}, {x:400, y:80}];
+var bullets=[{}];
+var score=0;
+var bigEnemy = [{
+    x: 250,
+    y:100
+}];
 
 function displayHero(){
     document.getElementById('hero').style['top']= hero.y+"px";
@@ -13,8 +19,23 @@ function moveEnemies(){
     for(var i=0; i<enemies.length; i++){
         enemies[i].y += 5;
        
-        if(enemies[i].y>500){
-            console.log("El enemigo pasó el límite");
+        if(enemies[i].y>550){
+            enemies[i].y=0;
+            enemies[i].x=Math.random()*500;
+        }
+    }
+}
+function displayScore(){
+    document.getElementById('score').innerHTML=score;
+}
+
+function moveBullets(){
+    for(var i=0; i<bullets.length; i++){
+        bullets[i].y -= 5;
+
+        if (bullets[i].y<0) {
+            bullets[i]=bullets[bullets.length-1];
+            bullets.pop();
         }
     }
 }
@@ -22,8 +43,42 @@ function gameLoop(){
     displayHero();
     moveEnemies();
     displayEnemies();
+    moveBullets();
+    displayBullets();
+    colision();
+    displayColisionEnemigo();
+    displayScore();
 }
-setInterval(gameLoop, 300);
+setInterval(gameLoop, 200);
+
+function colision(){
+    for(var i=0; i<bullets.length; i++){
+        for(var j=0; j<enemies.length; j++){
+            if (Math.abs(bullets[i].x - enemies[j].x)<10 && Math.abs(bullets[i].y - enemies[j].y)<10) {
+                score+=10;
+                enemies.pop();
+            }
+        }   
+    }    
+}
+function displayColisionEnemigo(){
+    for(var i=0; i<hero.length; i++){
+        for(var j=0; j<enemies.length; j++){
+            if (Math.abs(hero[i].x - enemies[j].x)<5 && Math.abs(hero[i].y - enemies[j].y)<5) {
+                score-=500;
+                console.log("hicieron la chocación");
+            }
+        }
+    }
+}
+
+function displayBullets(){
+    output='';
+    for(var i=0; i<bullets.length; i++){
+        output+="<div class='bullet' style='top:"+bullets[i].y+"px; left:"+bullets[i].x+"px;'></div>";
+    }
+    document.getElementById('bullets').innerHTML=output;
+}
 
 function displayEnemies(){
     var output='';
@@ -43,8 +98,9 @@ document.onkeydown= function(e){
         hero.y -= 10;
     } else if(e.keyCode == 40){
         hero.y += 10;
+    } else if(e.keyCode == 32){//Bullets
+        bullets.push({x: hero.x, y: hero.y});
+        displayBullets();
     }
-    displayHero();
-    displayEnemies();
 }
 displayHero();
